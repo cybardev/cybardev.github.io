962cf3851b50a7f40049cc7f92f06103ea0eb072
cybardev <50134239+cybardev@users.noreply.github.com>
2025-07-06 10:49:59 -0300

    add shell utility for zola serve

--- shell.nix
+++ shell.nix
@@ -1,8 +1,12 @@
 {
   pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
 }:
 pkgs.mkShell {
   packages = with pkgs; [
     zola
+    (writeShellScriptBin "zload" ''
+      ${lib.getExe zola} serve --interface 0.0.0.0 --port 10101 --base-url /
+    '')
   ];
 }
