55259142b4a9e1e9d2eb4b5237048c3ad1c17b45
cybardev <50134239+cybardev@users.noreply.github.com>
2025-07-06 10:49:45 -0300

    improve syntax

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "A Nix-flake-based Zola development environment";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
   outputs =
-    inputs:
+    {nixpkgs, ...}:
     let
       forEachSupportedSystem =
         f:
-        inputs.nixpkgs.lib.genAttrs inputs.nixpkgs.lib.systems.flakeExposed (
-          system: f { pkgs = import inputs.nixpkgs { inherit system; }; }
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: f { pkgs = import nixpkgs { inherit system; }; }
         );
     in
     {
