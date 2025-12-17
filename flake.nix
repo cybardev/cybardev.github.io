b259194bc8344737e4b4b28c8626e1bd4d1c6d70
cybardev <50134239+cybardev@users.noreply.github.com>
2025-12-16 23:49:58 -0400

    feat: use only flake.nix for devshell

--- flake.nix
+++ flake.nix
@@ -4,17 +4,28 @@
   outputs =
     { nixpkgs, ... }:
     let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forEachSupportedSystem =
-        f:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-          system: f { pkgs = import nixpkgs { inherit system; }; }
-        );
+        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
       devShells = forEachSupportedSystem (
         { pkgs }:
         {
-          default = import ./shell.nix { inherit pkgs; };
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              wrangler
+              zola
+              (writeShellScriptBin "zload" ''
+                ${pkgs.lib.getExe zola} serve --interface 0.0.0.0 --port 10101 --base-url /
+              '')
+            ];
+          };
         }
       );
     };
